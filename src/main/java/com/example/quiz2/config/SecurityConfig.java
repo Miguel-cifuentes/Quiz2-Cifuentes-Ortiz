ed6f9bcc8a2cf0fcbd933f1c687ec5e9c594665d
Joseph Mateo Ortiz Cortes <ortizcortesjosephmateo@gmail.com>
2026-04-14 09:52:05 -0500

    feat: implementación de role

--- src/main/java/com/example/quiz2/config/SecurityConfig.java
+++ src/main/java/com/example/quiz2/config/SecurityConfig.java
@@ -1,4 +1,65 @@
 package com.example.quiz2.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final UserDetailsService userDetailsService;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+
+                        //  públicos
+                        .requestMatchers("/auth/**").permitAll()
+
+                        //  ADMIN
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+
+                        //  CHEF
+                        .requestMatchers("/chef/**").hasRole("CHEF")
+
+                        //  resto
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authenticationProvider(authenticationProvider())
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider =
+                new DaoAuthenticationProvider(userDetailsService);
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration config
+    ) throws Exception {
+        return config.getAuthenticationManager();
+    }
 }
---
+++ src/main/java/com/example/quiz2/entities/Role.java
@@ -0,0 +1,6 @@
+package com.example.quiz2.entities;
+
+public enum Role {
+    ADMIN,
+    CHEF
+}
